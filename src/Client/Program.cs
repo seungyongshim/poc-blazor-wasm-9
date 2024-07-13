eb82d766550fb2949835309f8f7f251382a3b688
seungyongshim <seungyongshim@gmail.com>
2024-07-13 16:48:43 +0900

    .

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -8,4 +8,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddOidcAuthentication(options =>
+{
+    options.ProviderOptions.Authority = "https://dev-5ureawdsacbjt2tc.us.auth0.com";
+    options.ProviderOptions.ClientId = "p4Rubx3E4mzRwwuePXEey5n93t697g6z";
+});
+
 await builder.Build().RunAsync();
