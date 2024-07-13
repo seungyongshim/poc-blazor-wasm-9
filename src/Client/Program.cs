8365a3a9eec75790d9b28f37679b0a72719d9844
seungyongshim <seungyongshim@gmail.com>
2024-07-13 17:33:59 +0900

    .

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,17 +1,24 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient("ServerAPI", client =>
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ServerAPI"));
 
 builder.Services.AddOidcAuthentication(options =>
 {
     options.ProviderOptions.Authority = "https://dev-5ureawdsacbjt2tc.us.auth0.com";
     options.ProviderOptions.ClientId = "p4Rubx3E4mzRwwuePXEey5n93t697g6z";
+    options.ProviderOptions.ResponseType = "code";
 });
 
 await builder.Build().RunAsync();
